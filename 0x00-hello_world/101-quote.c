69e1621b5523023b1b4e7c2aa078141d824a69a8
evansmwenda <evansmwenda.em@gmail.com>
2022-12-08 07:35:36 -0800

    Add optional tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,13 @@
 /**
  * main - entry point
  *
- * Description: prints "and that piece of art is useful" to standard error
+ * Description: prints "and that piece of art is useful" to standard error description
  *
  * return: Always 1 (Error)
  */
 
 int main(void)
 {
-	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",60);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);	
 }
