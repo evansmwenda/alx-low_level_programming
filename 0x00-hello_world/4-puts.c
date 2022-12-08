d6aa7fec656663483221501655e5ea21188b0db3
evansmwenda <evansmwenda.em@gmail.com>
2022-12-08 04:16:43 -0800

    Update all files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
- *main - entry point of program
- *Description - prints text and returns with 0 (success)
- *return - returns success
+ * main - entry point of program
+ * 
+ * Description - prints text and returns with 0 (success)
+ *
+ * return - returns success
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
