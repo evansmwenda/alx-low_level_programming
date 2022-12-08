057857012c4e9c5d5f46c36cb7e2ed05ab367bf9
evansmwenda <evansmwenda.em@gmail.com>
2022-12-08 03:44:32 -0800

    Add 4 puts c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ *Description - prints text and returns with 0 (success)
+ * */
+int main(){
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
