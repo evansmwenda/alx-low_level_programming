a5be93a26bacca5b272290b0a8a5d4f8daff7501
evansmwenda <evansmwenda.em@gmail.com>
2022-12-08 04:45:48 -0800

    Update alld files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*
- * main: Entry point
- *
- * Description: prints with proper grammar text
+/**
+ * main: prints the string with proper grammar *
+ * Description: prints with proper grammar text but the outcome is art
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
