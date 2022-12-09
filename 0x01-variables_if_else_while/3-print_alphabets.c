48f47054babc9aea0156b80e7fbdf840a5ab95ca
evansmwenda <evansmwenda.em@gmail.com>
2022-12-09 02:10:35 -0800

    Add 0-7 files

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,15 +10,15 @@
 
 int main(void)
 {
-        char i, j;
+	char i, j;
 
         for (i = 'a'; i <= 'z'; i++)
         {
-                putchar(i);
+		putchar(i);
         }
 	for (j = 'A'; j <= 'Z'; j++)
         {
-                putchar(j);
+		putchar(j);
         }
 	putchar('\n');
         return (0);
