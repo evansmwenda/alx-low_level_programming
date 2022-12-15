75ea1e038f49e88d3e76f52d3a1e10fd6cc8003a
evansmwenda <evansmwenda.em@gmail.com>
2022-12-15 09:46:02 -0800

    Add files

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,12 @@
 
 void print_most_numbers(void)
 {
-	int i = 0;
+	int c;
 
-	for (i = 0; i < 9; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (i != 2 &&  i != 4)
-			_putchar(i);
-		_putchar('\n');
+		if (c != '2' && c != '4')
+			_putchar(c);
 	}
+	_putchar('\n');
 }
