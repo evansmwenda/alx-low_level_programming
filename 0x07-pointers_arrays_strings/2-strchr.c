75d90a9de1e577d5de629b228b92997796f3b1ba
evansmwenda <evansmwenda.em@gmail.com>
2023-01-03 10:32:11 -0800

    Add files

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,19 +7,20 @@
 * Return: pointer
 */
 
+
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-	
-	while (s[i] != '\0')
+
+	while (*(s + i) != '\0')
 	{
-		if (s[i] == c)
+		if (*(s + i) == c)
 		{
 			return (s + i);
 		}
+
 		i++;
 	}
-
 	if (c == '\0')
 	{
 		return (s + i);
