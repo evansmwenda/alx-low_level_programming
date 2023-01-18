03ba0bccb2647672a5fbc8fe117ca867d62e9b27
evansmwenda <evansmwenda.em@gmail.com>
2023-01-18 12:55:38 -0800

    initial commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main()
+/**
+ * main - prints name of file it was compiled from
+ * Return: 0 (Always)
+ */
+
+
+int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
+	return (0);
 }
 
